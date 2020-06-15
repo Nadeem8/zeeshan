ad4f09dd51a6e495df0a46f08da99bbc1a15c3ef
Nadeem8 <66934923+Nadeem8@users.noreply.github.com>
2020-06-15 10:52:32 +0500

    Update and rename zeroboy.py to freefire.py

--- freefire.py
+++ freefire.py
@@ -49,9 +49,9 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - NADEEM -OFFICAL------•◈•
+|__\~~) •||•AHMAD - RAJPUT---------------•◈•
+|__(-----\  •◈•------FREEFIRE-GAMING--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
+\033[1;96mAuthor  \NADEEM.AHMAD.FREEFIRE.GAMER
+\033[1;96mInstagram \NADEEM7080
+\033[1;96mFacebook  \NADEEM AHMAD
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = nadeem
+CorrectPassword = nadeem
 
 loop = 'true'
 while (loop == 'true'):
